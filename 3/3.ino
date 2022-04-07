63a69ac3c4a1f7293c9be61ae11571f3ae49fd60
Eyal Rufeisen <eyalrocks@gmail.com>
2022-04-07 16:29:12 +0200

    before deleting class

--- 3/3.ino
+++ 3/3.ino
@@ -1,13 +1,13 @@
 /*
- * Created by Eyal Rufeisen
- *
- * Based on example code from: https://arduinogetstarted.com/tutorials/arduino-rfid-nfc
- */
+   Created by Eyal Rufeisen
+
+   Based on example code from: https://arduinogetstarted.com/tutorials/arduino-rfid-nfc
+*/
 
 /* Required Libraries -> If you get an error about not finding these libraries ->
-Go to tools -> Manage Libraries and search for 'MFRC522' (and LinkedList) -> install them. 
-SPI should be installed by default 
-*/ 
+  Go to tools -> Manage Libraries and search for 'MFRC522' (and LinkedList) -> install them.
+  SPI should be installed by default
+*/
 #include <SPI.h>
 #include <MFRC522.h>
 #include <LinkedList.h>
@@ -17,12 +17,12 @@ SPI should be installed by default
 #define RST_PIN 5
 MFRC522 rfid(SS_PIN, RST_PIN);
 
-byte savedUID[4] = {0x29,0x18,0x66,0xB3};
+byte savedUID[4] = {0x29, 0x18, 0x66, 0xB3};
 class UidEntry {
   public:
-    byte *uid;
+    byte uid[4];
 };
-
+LinkedList<UidEntry> myUIDS;
 
 void setup() {
   // Initialization stuff
@@ -35,10 +35,10 @@ void setup() {
   printArrAsHex(savedUID, 4);
   Serial.println();
 
-  UidEntry *preSaved = new UidEntry();
-  preSaved->uid = savedUID;
+  UidEntry preSaved = new UidEntry();
+  preSaved.uid = savedUID;
 
-  LinkedList<UidEntry*> myUIDS = LinkedList<UidEntry*>();
+  myUIDS = LinkedList<UidEntry*>();
   myUIDS.add(preSaved);
 
   Serial.println("Tap RFID/NFC Tag on reader");
@@ -50,7 +50,7 @@ void loop() {
       MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
 
       // Read rfid into our own arr
-      byte currUID[4] = {0,0,0,0};
+      byte currUID[4] = {0, 0, 0, 0};
       copyByteArr(currUID, rfid.uid.uidByte, 4);
 
       // Print current UID
@@ -59,7 +59,7 @@ void loop() {
       printArrAsHex(currUID, 4);
       Serial.println();
 
-      if (compareByteArr(savedUID, currUID, 4)) {
+      if (UIDExistsInList(myUIDS, currUID)) {
         Serial.print("Authorized.");
         Serial.println();
       }
@@ -76,23 +76,23 @@ void loop() {
 }
 
 /*
- * Prints the hexadecimal representation of the rfid/nfc UID - code
- * arr - The byte array to sace the UID into
- * arrSize - size of UID code to print
- */
+   Prints the hexadecimal representation of the rfid/nfc UID - code
+   arr - The byte array to sace the UID into
+   arrSize - size of UID code to print
+*/
 void printArrAsHex(byte arr[], int arrSize) {
-    for (int i = 0; i < arrSize; i++) {
-      Serial.print(arr[i] < 0x10 ? " 0" : " ");
-      Serial.print(arr[i], HEX);
-    }
+  for (int i = 0; i < arrSize; i++) {
+    Serial.print(arr[i] < 0x10 ? " 0" : " ");
+    Serial.print(arr[i], HEX);
+  }
 }
 
 /*
- * Copys/reads rfid bytes into the array byte by byte
- * copyInto - copys the bytes into this array
- * copyFrom - copys the bytes from this array
- * arrSize - array size
- */
+   Copys/reads rfid bytes into the array byte by byte
+   copyInto - copys the bytes into this array
+   copyFrom - copys the bytes from this array
+   arrSize - array size
+*/
 void copyByteArr (byte copyInto[], byte copyFrom[], int arrSize) {
   for (int i = 0; i < arrSize; i++) {
     copyInto[i] = copyFrom[i];
@@ -100,11 +100,11 @@ void copyByteArr (byte copyInto[], byte copyFrom[], int arrSize) {
 }
 
 /*
- * Compares two byte arrays - returns true if they're indentical, returns false otherwise.
- * arr1 - first byte array to compare
- * arr2 - second byte array to compare
- * arrSize - size of the arrays
- */
+   Compares two byte arrays - returns true if they're indentical, returns false otherwise.
+   arr1 - first byte array to compare
+   arr2 - second byte array to compare
+   arrSize - size of the arrays
+*/
 bool compareByteArr (byte arr1[], byte arr2[], int arrSize) {
   for (int i = 0; i < arrSize; i++) {
     if (arr1[i] != arr2[i]) {
@@ -113,4 +113,31 @@ bool compareByteArr (byte arr1[], byte arr2[], int arrSize) {
   }
 
   return true;
+}
+
+bool compareByteArr2 (byte arr1[], byte* arr2, int arrSize) {
+  for (int i = 0; i < arrSize; i++) {
+    if (arr1[i] != arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+/*
+
+*/
+bool UIDExistsInList (LinkedList<UidEntry*> UIDS, byte uidToSearch[]) {
+  for (int i = 0; i < UIDS.size(); i++) {
+    UidEntry* currUIDEntry = UIDS.get(i);
+    byte* currCompared = currUIDEntry->uid;
+
+    if (compareByteArr2(uidToSearch, currCompared, 4))
+    {
+      return true;
+    }
+  }
+
+  return false;
 }
